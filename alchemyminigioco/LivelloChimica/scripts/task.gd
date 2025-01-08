a21e8ed7fd930305a7c4f2d543217d2dad7af753
Oleksandr Kushnir <oleksandr.kushnirjob@gmail.com>
2025-01-08 22:46:08 +0100

    ....

---
+++ alchemyminigioco/LivelloChimica/scripts/task.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+
+var lvl1 : String
+var lvl2 : String
+var counterlvl = 0
+var base : String
+var completato = false
+var open = true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = base
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if completato == true:
+		$Label.text = "completato"
+		$Button.hide()
+	pass
+
+
+func _on_button_button_down() -> void:
+	counterlvl = counterlvl + 1
+	if(counterlvl == 1):
+		$Label.text = $Label.text + " --> " +lvl1 
+	else:
+		$Label.text = base + " --> " + lvl2
+		$Button.hide()
+		$Label2.hide()
+	pass # Replace with function body.
+
+
+func _on_scomparsa_button_down() -> void:
+	if(open):
+		self.move_local_x(400)
+		open = false;
+	else:
+		self.move_local_x(-400)
+		open = true;
+	pass # Replace with function body.
---
+++ alchemyminigioco/LivelloChimica/scripts/tutorial.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var handled = 0  # Aggiungi la variabile "handled" per tenere traccia dello stato
+@onready var compiti_e_indizi: Node2D = $Compiti
+@onready var elementi: Node2D = $Elementi
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	compiti_e_indizi.hide()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey or event is InputEventMouseButton :
+		if (event.is_pressed() and event.as_text() == "Enter") or event.is_pressed():  # Verifica se Enter è stato premuto
+			if handled == 0:
+				 # Azioni quando il tasto Enter è premuto la prima volta
+				elementi.free()
+				compiti_e_indizi.show()
+				handled += 1  # Incrementa "handled" per evitare che il codice venga eseguito più volte
+			elif handled == 1:
+			# Azioni quando il tasto Enter è premuto la seconda volta
+				compiti_e_indizi.free()
+				handled += 1  # Incrementa "handled" per evitare ulteriori esecuzioni
+
+
+func _on_area_2d_mouse_entered() -> void:
+	pass # Replace with function body.
