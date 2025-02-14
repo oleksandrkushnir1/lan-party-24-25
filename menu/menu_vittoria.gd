898bcf8be198b9a503d888e2ece5d9eb15784b9f
StefanoGavatta <stefano.gavatta@marconirovereto.it>
2025-02-14 21:38:49 +0100

    menu terminato

--- menu/menu_vittoria.gd
+++ menu/menu_vittoria.gd
@@ -1,17 +1,20 @@
 extends Control
-@onready var tempo_impiegato: Label = $Control/TempoTab/DescriptionTempo/containerdati/TempoImpiegato
-@onready var bricoguadagnati: Label = $Control/CoinTab/DescriptionBricoCoin/containerdati/Bricoguadagnati
-@onready var punteggioottenuto: Label = $Control/PointTab/Descriptionpunteggio/containerdati/punteggioottenuto
+@onready var tempo_impiegato: Label = $Card/TempoTab/DescriptionTempo/containerdati/TempoImpiegato
+@onready var bricoguadagnati: Label = $Card/CoinTab/DescriptionBricoCoin/containerdati/Bricoguadagnati
+@onready var punteggioottenuto: Label = $Card/PointTab/Descriptionpunteggio/containerdati/punteggioottenuto
 
 @onready var titolo: Label = $TitoloSprite/titolo
 
 @onready var bottonedes: TextureButton = $bottonedes
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	compila_scheda(randi_range(100,200))
-
+	
 func compila_scheda(tempoInt:int, Vittoria: bool = false):
+	get_tree().paused = true
+	$".".visible = true
 	var text = func():
 		if !Vittoria:
 			return "sconfitta"
@@ -20,4 +23,12 @@ func compila_scheda(tempoInt:int, Vittoria: bool = false):
 	tempo_impiegato.text = str(tempoInt)
 	bricoguadagnati.text = str(randi_range(100,400))
 	punteggioottenuto.text = str(randi_range(1000,6000))
-	$AnimationPlayer.play("apparizione")
+	animation_player.play("apparizione")
+	
+func cancella_menu():
+	animation_player.play_backwards("apparizione")
+	await animation_player.animation_finished
+	queue_free()
+
+func _on_bottonedes_pressed() -> void:
+	cancella_menu()
